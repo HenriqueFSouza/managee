b5759dcd9920b8fab4579a2e8815026ee929fe6a
Henrique Francisco de Souza <henriquesouza432@outlook.com>
2024-07-18 16:43:30 -0300

    option to the database url

--- mf-auth/src/database/index.ts
+++ mf-auth/src/database/index.ts
@@ -14,7 +14,7 @@ class Database {
   }
   async mongo() {
     try {
-      await mongoose.connect(process.env.MONGODB_URL!);
+      await mongoose.connect(process.env.MONGODB_URL! || '');
       console.log('Connected to MongoDB');
     } catch (err) {
       console.error('Error connecting to MongoDB', err);
