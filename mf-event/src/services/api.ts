934722d854838a442bbcb81e1500604ecc65e826
Henrique Francisco de Souza <henriquesouza432@outlook.com>
2024-07-19 15:17:12 -0300

    fix: changes

--- mf-event/src/services/api.ts
+++ mf-event/src/services/api.ts
@@ -4,3 +4,10 @@ export const api = axios.create({
   baseURL: process.env.API_URL || 'https://managee.onrender.com',
   withCredentials: true
 })
+
+api.interceptors.request.use(async (config) => {
+  const userData = await localStorage.getItem('managee:userData')
+  const token = userData && JSON.parse(userData).token
+  config.headers.Authorization = `Bearer ${token}`
+  return config
+})
