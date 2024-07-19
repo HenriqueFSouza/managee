23b4434ef181c36ad9ad51392713511501a6f71e
Henrique Francisco de Souza <henriquesouza432@outlook.com>
2024-07-19 09:34:14 -0300

    final config for deployment

--- container/webpack.config.js
+++ container/webpack.config.js
@@ -5,7 +5,7 @@ const deps = require("./package.json").dependencies;
 
 module.exports = (_, argv) => ({
   output: {
-    publicPath: "http://localhost:3000/",
+    publicPath: argv.mode === "development"  ? "http://localhost:3000/" : "https://managee-mf-container.onrender.com/",
   },
   resolve: {
     extensions: [".tsx", ".ts", ".jsx", ".js", ".json"],
