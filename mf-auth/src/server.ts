0f8e8871191683dbe92d4f218a88592efaaccfea
Henrique Francisco de Souza <henriquesouza432@outlook.com>
2024-07-19 16:22:28 -0300

    added logs to the requests

--- mf-auth/src/server.ts
+++ mf-auth/src/server.ts
@@ -5,3 +5,9 @@ import app from "./app";
 app.listen(process.env.PORT, () => {
   console.log(`Server is runnig on port ${process.env.PORT}`);
 });
+
+app.use((req, res, next) => {
+  console.log('Request Origin:', req.headers.origin);
+  console.log('Request Headers:', req.headers);
+  next();
+});
