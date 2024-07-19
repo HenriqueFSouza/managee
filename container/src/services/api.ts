8edf75165cd3e331de583cf06dc4f88d63d43ab5
Henrique Francisco de Souza <henriquesouza432@outlook.com>
2024-07-19 13:21:03 -0300

    fix: baseUrl for axios

--- container/src/services/api.ts
+++ container/src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: process.env.API_URL
+  baseURL: process.env.API_URL || 'https://managee.onrender.com'
 })
 
 api.interceptors.request.use(async (config) => {
